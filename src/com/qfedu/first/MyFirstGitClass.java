26277060962bcbdfe5903642dba82b02b206793a
zhFinal <zhqq6736@163.com>
2019-04-10 10:07:34 +0800

    123

--- src/com/qfedu/first/MyFirstGitClass.java
+++ src/com/qfedu/first/MyFirstGitClass.java
@@ -3,5 +3,6 @@ package com.qfedu.first;
 public class MyFirstGitClass {
     public static void main(String[] args) {
         System.out.println("main方法");
+        System.out.println("大幅度修改");
     }
 }
